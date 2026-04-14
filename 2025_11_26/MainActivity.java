3dfc7574a87466f7fe6f346e107a2613ea841c3c
Nikodem Szadejko <szadejko.nikodem@gmail.com>
2026-04-14 23:32:51 +0200

    Add files via upload

---
+++ 2025_11_26/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.firstjava;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button button1 = findViewById(R.id.button2);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText entry = findViewById(R.id.editTextText2);
+                Toast.makeText(MainActivity.this, entry.getText().toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public void przepiszeTekst(View v) {
+        EditText entry = findViewById(R.id.editTextText);
+        TextView output = findViewById(R.id.outputText);
+        output.setText(entry.getText().toString());
+    }
+}
